6d83f6b9969eb7716d8863458453807b3192a370
JordanDDisch <jordanddisch@gmail.com>
2019-06-20 10:31:42 -0500

    removed fileuuid

--- demo/src/index.js
+++ demo/src/index.js
@@ -9,7 +9,7 @@ class Demo extends Component {
     super(props)
 
     this.state = {
-      fileUUIDs: ['d1103c6b-dc61-429b-9505-8d4f02b64118']
+      fileUUIDs: []
     }
 
     this.onChange = this.onChange.bind(this)
